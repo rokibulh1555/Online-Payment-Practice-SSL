8e5cdfd2099922c9d6dcde2c58e505ad0dff35ec
rokib <rokibulh1555@gmail.com>
2026-03-30 09:19:14 +0600

    committed package files.

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+    globalIgnores(['dist', 'node_modules']),
+    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+},
+parserOptions: {
+    ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+    sourceType: 'module',
+},
+},
+plugins: {
+    'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+},
+rules: {
+...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+    ],
+        'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+        'no-undef': 'error',
+},
+},
+])
